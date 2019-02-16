8edb7e9e601c489c25b1f9b4f3478be5ef73b3ce
Akira Kosuda <sayuen0@gmail.com>
2019-02-16 12:37:21 +0900

    add: session controller and pages views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_or_create_from_auth(request.env["omniauth.auth"])
+    session[:user_id] = user.uid
+    flash[:notice] = "ユーザ認証が完了しました"
+    redirect_to root_path
+  end
+
+  # root path
+  def index
+  end
+
+  def destroy
+    reset_session
+    flash[:notice] ="ログアウトしました"
+    redirect_to root_path
+  end
+end
